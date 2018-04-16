7156bdd05c5026b79f1e14493afcb57a260fa078
Jonas Smith <30472987+JonasESmith@users.noreply.github.com>
2018-04-16 08:08:24 -0500

    Base files

---
+++ Task Form/SpoofMain.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace Task_Form
+{
+  partial class SpoofMain
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.nameTxt = new MetroFramework.Controls.MetroTextBox();
+      this.nameLabel = new MetroFramework.Controls.MetroLabel();
+      this.mailButton = new MetroFramework.Controls.MetroButton();
+      this.detailTxt = new MetroFramework.Controls.MetroTextBox();
+      this.detLabel = new MetroFramework.Controls.MetroLabel();
+      this.subLabel = new MetroFramework.Controls.MetroLabel();
+      this.subBox = new MetroFramework.Controls.MetroTextBox();
+      this.toLabel = new MetroFramework.Controls.MetroLabel();
+      this.recTxtBox = new MetroFramework.Controls.MetroTextBox();
+      this.spoofBox = new MetroFramework.Controls.MetroLabel();
+      this.spoofText = new MetroFramework.Controls.MetroTextBox();
+      this.messageLabel = new MetroFramework.Controls.MetroLabel();
+      this.textTest = new System.Windows.Forms.TextBox();
+      this.txtBoxCount = new MetroFramework.Controls.MetroTextBox();
+      this.labelCount = new MetroFramework.Controls.MetroLabel();
+      this.SuspendLayout();
+      // 
+      // nameTxt
+      // 
+      // 
+      // 
+      // 
+      this.nameTxt.CustomButton.Image = null;
+      this.nameTxt.CustomButton.Location = new System.Drawing.Point(173, 1);
+      this.nameTxt.CustomButton.Name = "";
+      this.nameTxt.CustomButton.Size = new System.Drawing.Size(21, 21);
+      this.nameTxt.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+      this.nameTxt.CustomButton.TabIndex = 1;
+      this.nameTxt.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+      this.nameTxt.CustomButton.UseSelectable = true;
+      this.nameTxt.CustomButton.Visible = false;
+      this.nameTxt.Lines = new string[0];
+      this.nameTxt.Location = new System.Drawing.Point(96, 65);
+      this.nameTxt.MaxLength = 32767;
+      this.nameTxt.Name = "nameTxt";
+      this.nameTxt.PasswordChar = '\0';
+      this.nameTxt.ScrollBars = System.Windows.Forms.ScrollBars.None;
+      this.nameTxt.SelectedText = "";
+      this.nameTxt.SelectionLength = 0;
+      this.nameTxt.SelectionStart = 0;
+      this.nameTxt.ShortcutsEnabled = true;
+      this.nameTxt.Size = new System.Drawing.Size(195, 23);
+      this.nameTxt.TabIndex = 1;
+      this.nameTxt.UseSelectable = true;
+      this.nameTxt.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+      this.nameTxt.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+      // 
+      // nameLabel
+      // 
+      this.nameLabel.AutoSize = true;
+      this.nameLabel.Location = new System.Drawing.Point(27, 65);
+      this.nameLabel.Name = "nameLabel";
+      this.nameLabel.Size = new System.Drawing.Size(63, 19);
+      this.nameLabel.TabIndex = 2;
+      this.nameLabel.Text = "full name";
+      // 
+      // mailButton
+      // 
+      this.mailButton.Location = new System.Drawing.Point(224, 363);
+      this.mailButton.Name = "mailButton";
+      this.mailButton.Size = new System.Drawing.Size(67, 23);
+      this.mailButton.TabIndex = 6;
+      this.mailButton.Text = "Send";
+      this.mailButton.UseSelectable = true;
+      this.mailButton.Click += new System.EventHandler(this.mailButton_Click);
+      // 
+      // detailTxt
+      // 
+      // 
+      // 
+      // 
+      this.detailTxt.CustomButton.Image = null;
+      this.detailTxt.CustomButton.Location = new System.Drawing.Point(33, 2);
+      this.detailTxt.CustomButton.Name = "";
+      this.detailTxt.CustomButton.Size = new System.Drawing.Size(159, 159);
+      this.detailTxt.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+      this.detailTxt.CustomButton.TabIndex = 1;
+      this.detailTxt.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+      this.detailTxt.CustomButton.UseSelectable = true;
+      this.detailTxt.CustomButton.Visible = false;
+      this.detailTxt.Lines = new string[0];
+      this.detailTxt.Location = new System.Drawing.Point(96, 181);
+      this.detailTxt.MaxLength = 32767;
+      this.detailTxt.Multiline = true;
+      this.detailTxt.Name = "detailTxt";
+      this.detailTxt.PasswordChar = '\0';
+      this.detailTxt.ScrollBars = System.Windows.Forms.ScrollBars.None;
+      this.detailTxt.SelectedText = "";
+      this.detailTxt.SelectionLength = 0;
+      this.detailTxt.SelectionStart = 0;
+      this.detailTxt.ShortcutsEnabled = true;
+      this.detailTxt.Size = new System.Drawing.Size(195, 164);
+      this.detailTxt.TabIndex = 5;
+      this.detailTxt.UseSelectable = true;
+      this.detailTxt.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+      this.detailTxt.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+      // 
+      // detLabel
+      // 
+      this.detLabel.AutoSize = true;
+      this.detLabel.Location = new System.Drawing.Point(27, 181);
+      this.detLabel.Name = "detLabel";
+      this.detLabel.Size = new System.Drawing.Size(46, 19);
+      this.detLabel.TabIndex = 5;
+      this.detLabel.Text = "details";
+      // 
+      // subLabel
+      // 
+      this.subLabel.AutoSize = true;
+      this.subLabel.Location = new System.Drawing.Point(27, 152);
+      this.subLabel.Name = "subLabel";
+      this.subLabel.Size = new System.Drawing.Size(51, 19);
+      this.subLabel.TabIndex = 7;
+      this.subLabel.Text = "Subject";
+      // 
+      // subBox
+      // 
+      // 
+      // 
+      // 
+      this.subBox.CustomButton.Image = null;
+      this.subBox.CustomButton.Location = new System.Drawing.Point(173, 1);
+      this.subBox.CustomButton.Name = "";
+      this.subBox.CustomButton.Size = new System.Drawing.Size(21, 21);
+      this.subBox.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+      this.subBox.CustomButton.TabIndex = 1;
+      this.subBox.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+      this.subBox.CustomButton.UseSelectable = true;
+      this.subBox.CustomButton.Visible = false;
+      this.subBox.Lines = new string[0];
+      this.subBox.Location = new System.Drawing.Point(96, 152);
+      this.subBox.MaxLength = 32767;
+      this.subBox.Name = "subBox";
+      this.subBox.PasswordChar = '\0';
+      this.subBox.ScrollBars = System.Windows.Forms.ScrollBars.None;
+      this.subBox.SelectedText = "";
+      this.subBox.SelectionLength = 0;
+      this.subBox.SelectionStart = 0;
+      this.subBox.ShortcutsEnabled = true;
+      this.subBox.Size = new System.Drawing.Size(195, 23);
+      this.subBox.TabIndex = 4;
+      this.subBox.UseSelectable = true;
+      this.subBox.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+      this.subBox.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+      // 
+      // toLabel
+      // 
+      this.toLabel.AutoSize = true;
+      this.toLabel.Location = new System.Drawing.Point(27, 123);
+      this.toLabel.Name = "toLabel";
+      this.toLabel.Size = new System.Drawing.Size(52, 19);
+      this.toLabel.TabIndex = 9;
+      this.toLabel.Text = "send to";
+      // 
+      // recTxtBox
+      // 
+      // 
+      // 
+      // 
+      this.recTxtBox.CustomButton.Image = null;
+      this.recTxtBox.CustomButton.Location = new System.Drawing.Point(173, 1);
+      this.recTxtBox.CustomButton.Name = "";
+      this.recTxtBox.CustomButton.Size = new System.Drawing.Size(21, 21);
+      this.recTxtBox.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+      this.recTxtBox.CustomButton.TabIndex = 1;
+      this.recTxtBox.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+      this.recTxtBox.CustomButton.UseSelectable = true;
+      this.recTxtBox.CustomButton.Visible = false;
+      this.recTxtBox.Lines = new string[0];
+      this.recTxtBox.Location = new System.Drawing.Point(96, 123);
+      this.recTxtBox.MaxLength = 32767;
+      this.recTxtBox.Name = "recTxtBox";
+      this.recTxtBox.PasswordChar = '\0';
+      this.recTxtBox.ScrollBars = System.Windows.Forms.ScrollBars.None;
+      this.recTxtBox.SelectedText = "";
+      this.recTxtBox.SelectionLength = 0;
+      this.recTxtBox.SelectionStart = 0;
+      this.recTxtBox.ShortcutsEnabled = true;
+      this.recTxtBox.Size = new System.Drawing.Size(195, 23);
+      this.recTxtBox.TabIndex = 3;
+      this.recTxtBox.UseSelectable = true;
+      this.recTxtBox.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+      this.recTxtBox.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+      // 
+      // spoofBox
+      // 
+      this.spoofBox.AutoSize = true;
+      this.spoofBox.Location = new System.Drawing.Point(27, 94);
+      this.spoofBox.Name = "spoofBox";
+      this.spoofBox.Size = new System.Drawing.Size(38, 19);
+      this.spoofBox.TabIndex = 11;
+      this.spoofBox.Text = "from";
+      // 
+      // spoofText
+      // 
+      // 
+      // 
+      // 
+      this.spoofText.CustomButton.Image = null;
+      this.spoofText.CustomButton.Location = new System.Drawing.Point(173, 1);
+      this.spoofText.CustomButton.Name = "";
+      this.spoofText.CustomButton.Size = new System.Drawing.Size(21, 21);
+      this.spoofText.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+      this.spoofText.CustomButton.TabIndex = 1;
+      this.spoofText.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+      this.spoofText.CustomButton.UseSelectable = true;
+      this.spoofText.CustomButton.Visible = false;
+      this.spoofText.Lines = new string[0];
+      this.spoofText.Location = new System.Drawing.Point(96, 94);
+      this.spoofText.MaxLength = 32767;
+      this.spoofText.Name = "spoofText";
+      this.spoofText.PasswordChar = '\0';
+      this.spoofText.ScrollBars = System.Windows.Forms.ScrollBars.None;
+      this.spoofText.SelectedText = "";
+      this.spoofText.SelectionLength = 0;
+      this.spoofText.SelectionStart = 0;
+      this.spoofText.ShortcutsEnabled = true;
+      this.spoofText.Size = new System.Drawing.Size(195, 23);
+      this.spoofText.TabIndex = 2;
+      this.spoofText.UseSelectable = true;
+      this.spoofText.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+      this.spoofText.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+      // 
+      // messageLabel
+      // 
+      this.messageLabel.AutoSize = true;
+      this.messageLabel.Location = new System.Drawing.Point(19, 365);
+      this.messageLabel.Name = "messageLabel";
+      this.messageLabel.Size = new System.Drawing.Size(0, 0);
+      this.messageLabel.TabIndex = 12;
+      // 
+      // textTest
+      // 
+      this.textTest.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+      this.textTest.Location = new System.Drawing.Point(19, 366);
+      this.textTest.Name = "textTest";
+      this.textTest.Size = new System.Drawing.Size(100, 20);
+      this.textTest.TabIndex = 7;
+      this.textTest.Visible = false;
+      // 
+      // txtBoxCount
+      // 
+      // 
+      // 
+      // 
+      this.txtBoxCount.CustomButton.Image = null;
+      this.txtBoxCount.CustomButton.Location = new System.Drawing.Point(41, 1);
+      this.txtBoxCount.CustomButton.Name = "";
+      this.txtBoxCount.CustomButton.Size = new System.Drawing.Size(21, 21);
+      this.txtBoxCount.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+      this.txtBoxCount.CustomButton.TabIndex = 1;
+      this.txtBoxCount.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+      this.txtBoxCount.CustomButton.UseSelectable = true;
+      this.txtBoxCount.CustomButton.Visible = false;
+      this.txtBoxCount.Lines = new string[0];
+      this.txtBoxCount.Location = new System.Drawing.Point(27, 322);
+      this.txtBoxCount.MaxLength = 32767;
+      this.txtBoxCount.Name = "txtBoxCount";
+      this.txtBoxCount.PasswordChar = '\0';
+      this.txtBoxCount.ScrollBars = System.Windows.Forms.ScrollBars.None;
+      this.txtBoxCount.SelectedText = "";
+      this.txtBoxCount.SelectionLength = 0;
+      this.txtBoxCount.SelectionStart = 0;
+      this.txtBoxCount.ShortcutsEnabled = true;
+      this.txtBoxCount.Size = new System.Drawing.Size(63, 23);
+      this.txtBoxCount.TabIndex = 13;
+      this.txtBoxCount.UseSelectable = true;
+      this.txtBoxCount.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+      this.txtBoxCount.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+      // 
+      // labelCount
+      // 
+      this.labelCount.AutoSize = true;
+      this.labelCount.Location = new System.Drawing.Point(27, 297);
+      this.labelCount.Name = "labelCount";
+      this.labelCount.Size = new System.Drawing.Size(41, 19);
+      this.labelCount.TabIndex = 14;
+      this.labelCount.Text = "count";
+      // 
+      // TaskForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(313, 406);
+      this.Controls.Add(this.labelCount);
+      this.Controls.Add(this.txtBoxCount);
+      this.Controls.Add(this.textTest);
+      this.Controls.Add(this.messageLabel);
+      this.Controls.Add(this.spoofBox);
+      this.Controls.Add(this.spoofText);
+      this.Controls.Add(this.toLabel);
+      this.Controls.Add(this.recTxtBox);
+      this.Controls.Add(this.subLabel);
+      this.Controls.Add(this.subBox);
+      this.Controls.Add(this.detLabel);
+      this.Controls.Add(this.detailTxt);
+      this.Controls.Add(this.mailButton);
+      this.Controls.Add(this.nameLabel);
+      this.Controls.Add(this.nameTxt);
+      this.Name = "TaskForm";
+      this.Text = "Masked Email";
+      this.Theme = MetroFramework.MetroThemeStyle.Dark;
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+    private MetroFramework.Controls.MetroTextBox nameTxt;
+    private MetroFramework.Controls.MetroLabel nameLabel;
+    private MetroFramework.Controls.MetroButton mailButton;
+    private MetroFramework.Controls.MetroTextBox detailTxt;
+    private MetroFramework.Controls.MetroLabel detLabel;
+    private MetroFramework.Controls.MetroLabel subLabel;
+    private MetroFramework.Controls.MetroTextBox subBox;
+    private MetroFramework.Controls.MetroLabel toLabel;
+    private MetroFramework.Controls.MetroTextBox recTxtBox;
+    private MetroFramework.Controls.MetroLabel spoofBox;
+    private MetroFramework.Controls.MetroTextBox spoofText;
+    private MetroFramework.Controls.MetroLabel messageLabel;
+    private System.Windows.Forms.TextBox textTest;
+    private MetroFramework.Controls.MetroTextBox txtBoxCount;
+    private MetroFramework.Controls.MetroLabel labelCount;
+  }
+}
+
---
+++ Task Form/SpoofMain.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+using System.Net.Mail;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+
+namespace Task_Form
+{
+  public partial class SpoofMain : MetroForm
+  {
+    public SpoofMain()
+    {
+      InitializeComponent();
+
+      this.Theme      = MetroFramework.MetroThemeStyle.Dark;
+      this.Style      = MetroFramework.MetroColorStyle.Blue;
+                      
+      nameTxt.Theme   = MetroFramework.MetroThemeStyle.Dark;
+      nameTxt.Style   = MetroFramework.MetroColorStyle.Blue;
+
+      nameLabel.Theme = MetroFramework.MetroThemeStyle.Dark;
+      nameLabel.Style = MetroFramework.MetroColorStyle.Blue;
+
+      mailButton.Theme = MetroFramework.MetroThemeStyle.Dark;
+      mailButton.Style = MetroFramework.MetroColorStyle.Blue;
+
+      detailTxt.Theme = MetroFramework.MetroThemeStyle.Dark;
+      detailTxt.Style = MetroFramework.MetroColorStyle.Blue;
+
+      detLabel.Theme = MetroFramework.MetroThemeStyle.Dark;
+      detLabel.Style = MetroFramework.MetroColorStyle.Blue;
+
+      subBox.Theme = MetroFramework.MetroThemeStyle.Dark;
+      subBox.Style = MetroFramework.MetroColorStyle.Blue;
+
+      subLabel.Theme = MetroFramework.MetroThemeStyle.Dark;
+      subLabel.Style = MetroFramework.MetroColorStyle.Blue;
+
+      spoofBox.Theme = MetroFramework.MetroThemeStyle.Dark;
+      spoofBox.Style = MetroFramework.MetroColorStyle.Blue;
+
+      spoofText.Theme = MetroFramework.MetroThemeStyle.Dark;
+      spoofText.Style = MetroFramework.MetroColorStyle.Blue;
+
+      toLabel.Theme = MetroFramework.MetroThemeStyle.Dark;
+      toLabel.Style = MetroFramework.MetroColorStyle.Blue;
+
+      recTxtBox.Theme = MetroFramework.MetroThemeStyle.Dark;
+      recTxtBox.Style = MetroFramework.MetroColorStyle.Blue;
+
+      messageLabel.Theme = MetroFramework.MetroThemeStyle.Dark;
+      messageLabel.Style = MetroFramework.MetroColorStyle.Blue;
+
+      txtBoxCount.Theme = MetroFramework.MetroThemeStyle.Dark;
+      txtBoxCount.Style = MetroFramework.MetroColorStyle.Blue;
+
+      labelCount.Theme = MetroFramework.MetroThemeStyle.Dark;
+      labelCount.Style = MetroFramework.MetroColorStyle.Blue;
+
+      // https://stackoverflow.com/questions/1357853/autocomplete-textbox-control
+      // https://stackoverflow.com/questions/9768938/change-the-bordercolor-of-the-textbox
+      textTest.BackColor = Color.Black;
+      textTest.ForeColor = Color.Gray;
+    }
+
+    private void send()
+    {
+      if (spoofText.Text.Contains("@") && (recTxtBox.Text.Contains("@carthagetigers.org")) && ((spoofText.Text.Contains(".com") || (spoofText.Text.Contains(".org")))))
+      {
+        string name = nameTxt.Text;
+        MailAddress from = new MailAddress(spoofText.Text, name);
+        MailAddress to = new MailAddress(recTxtBox.Text, "Name and stuff");
+        List<MailAddress> cc = new List<MailAddress>();
+        SendEmail(subBox.Text, from, to, cc);
+
+        messageLabel.Text = "Message sent";
+      }
+      else
+      {
+        messageLabel.Text = "eMail in wrong format";
+      }
+    }
+
+    protected void SendEmail(string _subject, MailAddress _from, MailAddress _to, List<MailAddress> _cc, List<MailAddress> _bcc = null)
+    {
+      int count = 1;
+      if (System.Text.RegularExpressions.Regex.IsMatch(txtBoxCount.Text, "  ^ [0-9]"))
+      {
+        txtBoxCount.Text = "";
+
+      }
+      else
+      {
+        count = Convert.ToInt32(txtBoxCount.Text);
+      }
+
+      string Text = "";
+      SmtpClient mailClient = new SmtpClient("mail.carthagetigers.org");
+      MailMessage msgMail;
+      Text = detailTxt.Text;
+      msgMail = new MailMessage();
+      msgMail.From = _from;
+      msgMail.To.Add(_to);
+
+      foreach (MailAddress addr in _cc)
+      {
+        msgMail.CC.Add(addr);
+      }
+      if (_bcc != null)
+      {
+        foreach (MailAddress addr in _bcc)
+        {
+          msgMail.Bcc.Add(addr);
+        }
+      }
+      msgMail.Subject = _subject;
+      msgMail.Body = Text;
+      msgMail.IsBodyHtml = true;
+      for(int i = 1; i <= count; i++)
+        mailClient.Send(msgMail);
+      msgMail.Dispose();
+    }
+
+    private void mailButton_Click(object sender, EventArgs e)
+    {
+      /// https://stackoverflow.com/questions/10940732/sending-emails-from-a-windows-forms-application
+      send();
+    }
+  }
+}
